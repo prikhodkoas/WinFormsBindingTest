b653c8b0a5d1ad68ef148437309006584e5959df
Alex <prikhodkoas@gmail.com>
2021-04-11 13:27:22 +0300

    Подписка на событие изменения текста в tb, для изменения модели.

---
+++ WinFormsBindingTest/BookInfo.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsBindingTest
+{
+    public class BookInfo
+    {
+        public string Author { get; set; }
+        public string Title { get; set; }
+        public string ISBN { get; set; }
+        public int PageCount { get; set; }
+        public string Publisher { get; set; }
+    }
+}
